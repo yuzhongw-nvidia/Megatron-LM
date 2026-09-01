e851e7d22d2ad4c936d90284b257912b76e84115
Yuzhong Wang <yuzhongw@nvidia.com>
2026-09-01 04:17:31 -0700

    [Dev] Add SiTU-GLU through Transformer Engine #6673
    Signed-off-by: Yuzhong Wang <yuzhongw@nvidia.com>

--- tests/unit_tests/transformer/test_te_fused_dense_mlp_spec.py
+++ tests/unit_tests/transformer/test_te_fused_dense_mlp_spec.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
 import pytest
 import torch.nn.functional as F
@@ -54,12 +54,12 @@ class TestTEFusedDenseMLPSpec:
 
     def test_wrong_activation_raises(self):
         config = _make_config(activation_func=F.gelu, gated_linear_unit=False)
-        with pytest.raises(ValueError, match="SwiGLU activation"):
+        with pytest.raises(ValueError, match="SwiGLU or SiTU-GLU activation"):
             TEFusedDenseMLP(config, _make_submodules())
 
     def test_gated_linear_unit_false_raises(self):
         config = _make_config(gated_linear_unit=False)
-        with pytest.raises(ValueError, match="SwiGLU activation"):
+        with pytest.raises(ValueError, match="SwiGLU or SiTU-GLU activation"):
             TEFusedDenseMLP(config, _make_submodules())
 
     def test_add_bias_linear_raises(self):
